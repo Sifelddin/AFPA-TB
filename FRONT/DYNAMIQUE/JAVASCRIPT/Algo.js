6e6f99f363014ff7b8aefc2f2a624a029ca46e65
Ali-Tarek <bekhouche3939@gmail.com>
2021-05-31 16:46:02 +0200

    C 15

--- FRONT/DYNAMIQUE/JAVASCRIPT/Algo.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/Algo.js
@@ -207,8 +207,6 @@
 
 // let grandFact = (phoNomber - 30) * 0.08  +  (10 * 0.10)  +  (20 * 0.09)
 
-
-
 //  if(phoNomber <= 10 ){
 //    alert(factDixpho)
     
@@ -251,3 +249,221 @@
 
 
 //Exercice 4.7
+
+//les tarifs  d'assurance automobile dans Une compagnie 
+// alert("les données qu'il faut rentrer sont en chiffre , les chiffres représentent les nombres d'années !")
+
+// let durée_entré = parseInt(prompt(" entré dans la maison depuis quand : "), 10);
+
+// if(durée_entré >= 1 ){
+    
+//     alert("un contrat de la couleur BLEU");
+
+//     console.log(durée_entré + " ans");
+
+// }else{
+    
+//     let age_client = parseInt(prompt("entrez l'age de client : "), 10);
+//     let durée_permis =  parseInt(prompt("titulaire du permis depuis quand : "), 10);
+//     let N_accident = parseInt(prompt("entrez nomber d'accident  : "), 10);
+    
+//     if( age_client < 25 && durée_permis < 2 && N_accident == 0 ||  age_client >= 25 &&  durée_permis < 2 && N_accident == 1  ||  age_client >= 25 &&  durée_permis >= 2 && N_accident == 2){
+//         alert("un contrat de la couleur ROUGE");
+//     }else if((age_client < 25 && durée_permis >= 2 || age_client >= 25 && durée_permis < 2) && N_accident == 0 || age_client >= 25 && durée_permis >= 2 && N_accident == 1){
+//         alert("un contrat de la couleur ORANGE");
+//     }else if(age_client >= 25 && durée_permis >= 2 && N_accident == 0){
+//         alert("un contrat de la couleur verte");
+//     }else{
+//         alert("le contrat est refusé");
+//     }
+
+//     console.log(age_client + " ans");
+//     console.log(durée_permis + " ans");
+//     console.log(N_accident + " ans");
+    
+// }
+
+
+
+
+//Exercice 4.8
+
+//  let jour = parseInt(prompt("enter le numéro de jour : ") , 10);
+//  let mois = parseInt(prompt("enter le numéro de mois : ") , 10);
+//  let année = parseInt(prompt("enter le numéro d'année : ") , 10);
+
+//  let anneé_bassextil ;
+
+
+
+// if(Number.isInteger(année/4) && Number.isInteger(année/100) == false || Number.isInteger(année/400)   ){
+//     anneé_bassextil = "bassextil";
+//     console.log(anneé_bassextil  )
+// }else{
+//     anneé_bassextil = "Nonbassextil";
+//     console.log(anneé_bassextil);
+// }
+
+//   if( jour >= 1 && jour <= 30  &&  ( mois == 4 || mois == 6 || mois == 9 || mois == 11) || jour >= 1 
+//   && jour <= 31 && (mois == 1 || mois == 3 || mois == 5 || mois == 7 || mois == 8 || mois == 10 || mois == 12 )
+//    ||(jour >= 1 && jour <= 28  || jour == 29 && anneé_bassextil == "bassextil") && mois == 2){
+   
+// alert("la date est valide " + jour + "/" + mois + "/" + année + "  " + "et l'année est  " + anneé_bassextil);
+//   }
+// else{
+//  alert("date note valide "+ jour + "/" + mois + "/" + année + ".");
+//  }
+
+//   console.log(jour + " jour");
+//   console.log(mois + " mois");
+//   console.log(année + " année");
+
+
+//Exercice 5.3
+//incrémenter  le chiffre entré par l'utilisateur
+
+// let nombreInitial = parseInt(prompt(" entrez un chiffre :"),10);
+
+// for(let i = 1 ; i <= 10 ; i++){
+
+//     nombreInitial++
+//     console.log(nombreInitial);
+// }
+
+
+
+//Exercice 5.4
+
+
+// let nombreInitial = parseInt(prompt(" entrez un chiffre :"),10);
+
+// let i = 0 ;
+// let produit ;
+// while(i < 10){
+//     i++
+// produit = nombreInitial + " x " + i + " = " + nombreInitial * i;
+
+
+// console.log(produit);
+// }
+
+
+//Exercice 5.5
+
+// calcule la somme des entiers en incrémentant jusqu'à nombreInitial
+
+
+// let nombreInitial = parseInt(prompt(" entrez un chiffre :"),10);
+// let somme = 0;
+// for(let i = 0; i <= nombreInitial; i++ ){
+
+//     somme += i ;
+// console.log(somme);
+// }
+
+
+
+//Exercice 5.6
+
+// function nomberInitial (num) {
+    
+//     if(num < 1){
+//         return 1
+//     }else{
+      
+//         return nomberInitial(num - 1) * num;
+//     }
+// }
+
+// console.log(nomberInitial(5));
+
+
+
+//  let nombreInitial = parseInt(prompt(" entrez un chiffre :"),10);
+//  let produit = 1 ;
+//  for(let i = 1; i <= nombreInitial; i++ ){
+
+//      produit *= i ;
+//  console.log(produit);
+//  }
+
+
+
+
+//Exercice 5.7
+
+//rentrer 20 nombres  successivement 
+
+// let arr = [];
+// let grandNum = 0;
+// let grandNumIndex = 0;
+// for(let i = 0 ; i < 20 ; i++){
+
+//     arr.push(nombres = parseInt(prompt(" entrez un chiffre :"),10));
+// }
+
+// grandNum = Math.max(...arr);
+// alert("Le plus grand de ces nombres est " + grandNum)
+ 
+// //trouver l'index de le grand chiffre
+
+// grandNumIndex =  arr.indexOf(grandNum);
+
+// alert("la position de ce nombre (index) est : " + grandNumIndex);
+
+
+// console.log(grandNum);
+// console.log(grandNumIndex);
+
+
+
+
+
+//Exercice 5.8
+
+//rentrer les chiffre  successivement  jusqu'a rentrer 0
+
+//  let arr = [];
+//  let grandNum = 0;
+//  let grandNumIndex = 0;
+//  nombres = parseInt(prompt(" entrez un chiffre :"),10)
+
+//  while(nombres !== 0){
+//     arr.push(nombres);
+//     nombres = parseInt(prompt(" entrez un chiffre :"),10)
+//  }
+// console.log(arr);
+//   grandNum = Math.max(...arr);
+//   alert("Le plus grand de ces nombres est " + grandNum)
+
+//   //trouver l'index de le grand chiffre
+
+//   grandNumIndex =  arr.indexOf(grandNum);
+
+//   alert("la position de ce nombre (index) est : " + grandNumIndex);
+
+
+//   console.log(grandNum);
+//   console.log(grandNumIndex);
+
+
+//Exercice 5.9
+
+
+let prixAchats = parseInt(prompt("rentrez la suite des prix :"),10) ;
+let prixArticles = 0;
+let sommeRendre = 0;
+
+while(prixAchats !== 0){
+    prixArticles += prixAchats;
+    prixAchats = parseInt(prompt("rentrez la suite des prix :"),10)
+    console.log(prixArticles);
+}
+alert("le prix de tous les articles " + prixArticles + " €")
+
+let sommePaye = parseInt(prompt("rentrez une somme pour payer  :"),10) ;
+
+sommeRendre = sommePaye - prixArticles;
+
+alert("la remise de la monnaie à rendre " + sommeRendre + " €");
+
