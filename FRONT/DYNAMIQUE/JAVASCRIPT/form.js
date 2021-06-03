4e9729b481b7e9b7373dd784b2cd6d9b9984b6de
Bekhouche Ali-Tarek <63245287+Sifelddin@users.noreply.github.com>
2021-06-03 22:09:58 +0200

    C 17

---
+++ FRONT/DYNAMIQUE/JAVASCRIPT/form.js
@@ -0,0 +1,34 @@
+const societe = document.getElementById('soci');
+const persoContact = document.getElementById('percontact');
+const codeP = document.getElementById('CP');
+const ville = document.getElementById("ville");
+const email = document.getElementById("email");
+const form = document.getElementById("form");
+
+form.addEventListener('submit',(e)=>{
+    let messages = [];
+    if(societe.value == "" || societe.value == null){
+        messages.push("La Société doit comporter au moins 1 caractère.");
+    }
+    
+    if(persoContact.value == "" || persoContact.value == null){
+        messages.push("La Personne à contacter doit comporter au moins 1 caractère.");
+    }
+   
+   if(codeP.value.length < 5  ){
+        messages.push("Le Code postal doit comporter 5 caractères numériques.");
+    }
+    if(ville.value == "" || ville.value == null){
+        messages.push("La Ville doit comporter au moins 1 caractère.");
+    }
+    if(email.value == "" || email.value == null || email.value.indexOf('@') == -1 ){
+        messages.push("Le Email doit comporter au moins le caractère @.");
+    }
+    
+    if(messages.length > 0){
+     alert(messages.join(", "))
+        e.preventDefault()
+        
+    }
+
+})
