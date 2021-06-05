ef67a04a9ac2b652cfb962747fa7c44d8719675c
Bekhouche Ali-Tarek <63245287+Sifelddin@users.noreply.github.com>
2021-06-05 17:23:47 +0200

    C18

--- FRONT/DYNAMIQUE/JAVASCRIPT/form.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/form.js
@@ -6,25 +6,34 @@ const email = document.getElementById("email");
 const form = document.getElementById("form");
 const select = document.getElementById('Sujet');
 const textarea = document.getElementById('textarea');
-console.log(select.childNodes);
+const telephone = document.getElementById('tel');
+
 form.addEventListener('submit',(e)=>{
+   
     let messages = [];
     if(societe.value == "" || societe.value == null){
-        messages.push("La Société doit comporter au moins 1 caractère.");
+        messages.push("entrez le nom de la société s,v,p");
     }
     
     if(persoContact.value == "" || persoContact.value == null){
-        messages.push("La Personne à contacter doit comporter au moins 1 caractère.");
+        messages.push("entrez le nom de la personne à contacter s,v,p");
     }
    
    if(codeP.value.length < 5  ){
-        messages.push("Le Code postal doit comporter 5 caractères numériques.");
+        messages.push("Le Code postal doit comporter 5 caractères numériques s,v,p.");
+    }
+   if(telephone.value.length < 10  ){
+        messages.push("Le numéro de telephone doit comporter 10 caractères numériques s,v,p.");
+        
     }
     if(ville.value == "" || ville.value == null){
         messages.push("La Ville doit comporter au moins 1 caractère.");
     }
     if(email.value == "" || email.value == null || email.value.indexOf('@') == -1 ){
         messages.push("Le Email doit comporter au moins le caractère @.");
+    }
+    if(select.value != "" || select.value != null){
+        textarea.value = select.value + "," + textarea.value;
     }
     
     if(messages.length > 0){
