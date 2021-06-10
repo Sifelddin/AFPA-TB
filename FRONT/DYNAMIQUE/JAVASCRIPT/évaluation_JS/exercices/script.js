cfbbbaeef5c3804572e719d78f9eb12dac369dda
Bekhouche Ali-Tarek <63245287+Sifelddin@users.noreply.github.com>
2021-06-10 22:46:05 +0200

    C 23

--- FRONT/DYNAMIQUE/JAVASCRIPT/évaluation_JS/exercices/script.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/évaluation_JS/exercices/script.js
@@ -1,6 +1,79 @@
+const exercice1 =  document.querySelector('[data-exercice1]');
+const exercice2 =  document.querySelector('[data-exercice2]');
 const exercice3 = document.querySelector('[data-exercice3]');
 const exercice4 = document.querySelector('[data-exercice4]');
 const result = document.querySelector('h1');
+
+//Exercice 1 - Calcul du nombre de jeunes, de moyens et de vieux
+exercice1.addEventListener("click", ()=>{
+
+
+const allH1 = document.querySelectorAll('h1');
+let SaisirAge =parseInt(prompt("veuillez rentrer un chiffre qui va représenter l'age une personne : "),10)
+let tableau = [100];
+let compteJeune = 0;
+let compteMoyen = 0;
+let compteVieux = 0;
+
+while(SaisirAge != 100) {
+if(SaisirAge > 0 && SaisirAge < 100){
+    tableau.push(SaisirAge);
+}
+    SaisirAge = parseInt(prompt("veuillez rentrer un chiffre qui va représenter l'age une personne : "))
+} 
+for(let i = 0; i <tableau.length; i++){
+    if(tableau[i] > 40 && tableau[i] <= 100){
+    compteVieux++;
+} else if(tableau[i] > 0 && tableau[i] < 20){
+    compteJeune++;
+}else if(tableau[i] >= 20 && tableau[i] <= 40){
+    compteMoyen++;
+}
+}
+
+
+allH1[0].innerText =allH1[0].textContent + compteJeune;
+allH1[1].innerText =allH1[1].textContent + compteMoyen;
+allH1[2].innerText =allH1[2].textContent + compteVieux;
+console.log(compteVieux +"v");
+console.log(compteJeune + "j");
+console.log(compteMoyen +"m");
+console.log(tableau);
+})
+
+
+
+
+
+
+
+
+//Exercice 2 : Table de multiplication
+exercice2.addEventListener("click", ()=>{
+
+    
+
+let tableMultip = (num) => {
+    let i = 0;
+    let calcul = "";
+    do{
+   calcul +="<h1>" + i + " x " + num + " = " + i*num +"<br></h1>";
+        i++;
+        
+    }while(i <= 10);
+  
+return calcul;
+}
+console.log(tableMultip(6));
+
+})
+
+
+
+
+
+
+
 
 
 //Exercice 3 : recherche d'un prénom
@@ -27,6 +100,17 @@ let trouverPrenom = (arr) =>{
 console.log(trouverPrenom(tab));
 
 })
+
+
+
+
+
+
+
+
+
+
+
 
 
 
