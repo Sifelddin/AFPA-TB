e1754b32faee97d723a59f391ebce12e1f497eb7
Ali-Tarek <bekhouche3939@gmail.com>
2021-06-23 16:46:04 +0200

    first day PHP

---
+++ BACK/SERVEUR/PHP/demo.php
@@ -0,0 +1,24 @@
+<?php
+// $jeux_nom = ["mario", 'titrice',"phlipare"];
+
+
+// print_r ($jeux_nom );
+// echo "<br>";
+
+
+// function affiche ($jeu_tab){
+//    foreach($jeu_tab as $jeu  ){
+
+//        echo "le nom de jeu  $jeu <br>";
+//    }
+// }
+// affiche($jeux_nom);
+
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', dirname(__FILE__) . '/log.txt');
+error_reporting(E_ALL);
+
+require 'joe.com';
+
+?> 
---
+++ BACK/SERVEUR/PHP/exercice1.php
@@ -0,0 +1,76 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <?php
+
+
+
+ 
+//Exercice 1
+//   $i = 1;
+//   while($i < 150){
+
+//       echo $i . '<br>';
+//       $i+=2;
+//   };
+
+?>
+
+
+
+
+
+<?php
+//EXERCICE 2
+// $str = "Je dois faire des sauvegardes régulières de mes fichiers.";
+
+// $i = 0;
+// while ($i <= 500){
+  
+//   echo "$str-$i<br>";
+// $i++;
+// }
+?>
+
+
+<table >
+
+<tbody>
+
+
+</tbody>
+
+
+</table>
+<table>
+  <caption>Noms et valeurs des couleurs</caption>
+  <tbody>
+    <tr>
+      <th scope="col"><?php for($i = 0; $i <=12; $i++ ){echo $i;}?></th>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td><code>#51F6F6</code></td>
+  
+    </tr>
+    <tr>
+      <th scope="row">Ocre</th>
+      <td><code>#F6BC57</code></td>
+      
+    </tr>
+  </tbody>
+</table>
+
+
+
+</body>
+</html>
