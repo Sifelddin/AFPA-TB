0640ba2931e1413549dce6b8d232d86d6dd49326
Bekhouche Ali-Tarek <63245287+Sifelddin@users.noreply.github.com>
2021-06-24 09:15:23 +0200

    new php commit

--- BACK/SERVEUR/PHP/demo.php
+++ BACK/SERVEUR/PHP/demo.php
@@ -14,11 +14,25 @@
 // }
 // affiche($jeux_nom);
 
-ini_set('display_errors', 1);
-ini_set('log_errors', 1);
-ini_set('error_log', dirname(__FILE__) . '/log.txt');
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('log_errors', 1);
+// ini_set('error_log', dirname(__FILE__) . '/log.txt');
+// error_reporting(E_ALL);
 
-require 'joe.com';
+// require 'joe.com';
 
-?> 
+
+//Exercice
+//Ecrivez la fonction calculator() traitant les opérations d'addition, de soustraction, de multiplication et de division.
+
+
+// function calculator(int $chiff1, int $chiff2)
+// {
+//     $addition = $chiff1 + $chiff2;
+//     $soustraction = $chiff1 - $chiff2;
+//     $multiplication = $chiff1 * $chiff2;
+//     $division = $chiff1 * $chiff2;
+//     echo "l'opération entre : $chiff1 et $chiff2 <br>  addition : $addition <br> soustraction : $soustraction <br> multiplication $multiplication <br> division : $division";
+// }
+
+// calculator(5, 6);
--- BACK/SERVEUR/PHP/exercice1.php
+++ BACK/SERVEUR/PHP/exercice1.php
@@ -1,76 +1,89 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
+
 <body>
 
 
-    <?php
+  <?php
 
 
+  //Exercice 1
+  //   $i = 1;
+  //   while($i < 150){
 
- 
-//Exercice 1
-//   $i = 1;
-//   while($i < 150){
+  //       echo $i . '<br>';
+  //       $i+=2;
+  //   };
 
-//       echo $i . '<br>';
-//       $i+=2;
-//   };
-
-?>
-
+  ?>
 
 
 
 
-<?php
-//EXERCICE 2
-// $str = "Je dois faire des sauvegardes régulières de mes fichiers.";
 
-// $i = 0;
-// while ($i <= 500){
-  
-//   echo "$str-$i<br>";
-// $i++;
-// }
-?>
+  <?php
+  //EXERCICE 2
+  // $str = "Je dois faire des sauvegardes régulières de mes fichiers.";
 
+  // $i = 0;
+  // while ($i <= 500){
 
-<table >
-
-<tbody>
+  //   echo "$str-$i<br>";
+  // $i++;
+  // }
+  ?>
 
 
-</tbody>
+  <table>
+
+    <tbody>
 
 
-</table>
-<table>
-  <caption>Noms et valeurs des couleurs</caption>
-  <tbody>
-    <tr>
-      <th scope="col"><?php for($i = 0; $i <=12; $i++ ){echo $i;}?></th>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td><code>#51F6F6</code></td>
-  
-    </tr>
-    <tr>
-      <th scope="row">Ocre</th>
-      <td><code>#F6BC57</code></td>
-      
-    </tr>
-  </tbody>
-</table>
+    </tbody>
+
+
+  </table>
+
+
+  <table style="width: 100%;">
+    <thead>
+      <th style="border: 1px solid black;"></th>
+      <?php
+      for ($i = 0; $i <= 12; $i++) {
+        echo "<th style=\"border: 1px solid black;font-size: 1.3rem;\">" . $i  . "</th>";
+      }
+      ?>
+    </thead>
+    <tbody>
+      <?php for ($j = 0; $j <= 12; $j++) { ?>
+        <tr>
+          <th style="border: 1px solid black;font-size: 1.3rem;"><?= $j ?></th>
+          <?php for ($i = 0; $i <= 12; $i++) { ?>
+            <td style="border: 1px solid black; text-align: center;font-size: 1.3rem; "><?= $i * $j ?></td>
+          <?php  } ?>
+
+        </tr>
+      <?php } ?>
+
+
+    </tbody>
+
+
+  </table>
+
+
+
+
 
 
 
 </body>
+
 </html>
